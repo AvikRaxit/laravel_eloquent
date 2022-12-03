49182014d4ccf2c697152eabb15f2b926f60cd1d
Avik Rakshit <39942608+avikrakshit00@users.noreply.github.com>
2022-12-03 17:46:08 +0530

    add folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,9 +100,8 @@
                                         <label for="message-text" class="col-form-label">Status:</label>
                                         <select class="form-select" aria-label="Default select example">
                                             <option selected>Open this select menu</option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
+                                            <option value="1">Active</option>
+                                            <option value="2">Active</option>
                                         </select>
                                     </div>
                                   </form>
